5b2a00c19b91e96d2b48d25e773becdf2f03af5a
Diwansu <37077407+Diwansu@users.noreply.github.com>
2023-12-15 14:21:04 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// Progression 1: create a function and fetch the api using axios
+  function getData() {
+     const newsPortal = document.getElementById('newsPortal') ;
+
+     axios 
+     .get(
+       'https://gnews.io/api/v4/search?q=example&apikey=635e23b157d58c5cbbe1da1e7cdc4c7d'
+     )
+    .then((res)=>{
+      console.log(res) 
+       const articleContainer = res.data.article ;
+         articleContainer.forEach ((article)=>{
+           const articleStore = document.createElement('div') 
+             articleStore.setAttribute('id','article') ;
+             articleStore.setAttribute('class' ,'article') ;
+
+             const title = document.createElement('h3')
+             const content = document.createElement('p')
+             const img = document.createElement('img') 
+
+             title.innerText = article.title ;
+             content.innerText = article.content ;
+             img.setAttribute('src' , article.image) 
+
+             articleStore.append(title,img,content);
+             
+             newsPortal.appendChild(articleStore) ;
+         })
+    })
+  }
+
+  getData();
+ 
